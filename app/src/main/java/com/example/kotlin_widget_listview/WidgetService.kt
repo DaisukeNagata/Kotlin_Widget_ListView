1aafff22e92048f1127589ba37b0c2ef02b78cca
大祐永田 <dbank0208@gmail.com>
2021-05-26 22:33:58 +0900

    add

---
+++ app/src/main/java/com/example/kotlin_widget_listview/WidgetService.kt
@@ -0,0 +1,10 @@
+package com.example.kotlin_widget_listview
+
+import android.content.Intent
+import android.widget.RemoteViewsService
+
+class WidgetService : RemoteViewsService() {
+    override fun onGetViewFactory(intent: Intent): RemoteViewsFactory {
+        return DataProvider(this)
+    }
+}
